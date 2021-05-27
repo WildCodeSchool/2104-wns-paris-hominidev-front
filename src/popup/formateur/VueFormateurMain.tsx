0f52e37431f10f9fe2ec97a000f52b58f6d3df9b
Erik <erik@linaia.com>
2021-05-27 15:36:46 +0200

    VueFormateurMain WIP

---
+++ src/popup/formateur/VueFormateurMain.tsx
@@ -0,0 +1,15 @@
+import Timer from './Timer';
+import Classe from './Classe';
+import logo from '../../../assets/logo/logo.svg';
+
+const VueFormateurMain = () => {
+  return (
+    <div data-testid='vueFormateurMain'>
+      <img data-testid='vueFormateurMain-logo' src={logo} />
+      <Timer />
+      <Classe />
+    </div>
+  );
+};
+
+export default VueFormateurMain;
